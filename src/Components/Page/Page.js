c8986c7a7546ac9b3a3e9b796251e26c1646cd99
James Tubman <jamestubman@jamess-imac.home>
2020-02-21 21:27:14 +0000

    Adds bootstrap

---
+++ src/Components/Page/Page.js
@@ -0,0 +1,19 @@
+/**
+ *  Page js
+ */
+
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+import Header from './Header/Header'
+
+export default () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container></Container>
+        </div>
+
+
+    )
+}
